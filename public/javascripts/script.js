16c5acc6387713093585725ebe1cb49d469b5a10
Toni-Erik Martin <Toni-Erik Martin>
2017-09-17 16:27:25 +0300

    sticky header, minor fixes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -7,22 +7,10 @@ $(document).ready(function() {
 
 		//scroll without hash in url
 		$('html, body').animate({
-			'scrollTop': $target.offset().top
+			'scrollTop': $target.offset().top-50
 		}, 1000, 'swing');
 	});
 });
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-	//document.body.scrollTop for Chrome, Safari, Opera
-	//document.documentElement.scrollTop for IE, Firefox
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		document.getElementById("top-button").style.display = "block";
-	} else {
-		document.getElementById("top-button").style.display = "none";
-	}
-}
 
 function topFunction() {
 	$('html, body').animate({scrollTop: 0}, 1000);
